817593c06d9eea8cac859baf4dd71096020f9df3
JoshuaBlack0704 <knightrider0704@gmail.com>
2021-04-11 23:53:37 -0500

    Testing

--- Assets/UniverseDataGeneration/OctTreeTesting.cs
+++ Assets/UniverseDataGeneration/OctTreeTesting.cs
@@ -6,6 +6,7 @@ public class OctTreeTesting : MonoBehaviour
 {
     public int pointCode;
     public List<int> OctCoords;
+    public List<int> Connections;
     public int temp;
     public int cubeCode = 0;
     public int cubeContentCount;
@@ -18,12 +19,17 @@ public class OctTreeTesting : MonoBehaviour
         {
             temp = pointCode;
             OctCoords = new List<int>();
+            Connections.Clear();
 
             var point = uni.masterPointsDatabase[pointCode];
 
             foreach (var step in point.OctTreeAddress)
             {
                 OctCoords.Add(step);
+            }
+            foreach (var con in point.Connections)
+            {
+                Connections.Add(con.Id);
             }
         }
 
