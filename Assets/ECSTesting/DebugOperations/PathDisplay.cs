02a1d75ccfd2c087c519e8c1df47006aca5e7c8e
JoshuaBlack0704 <knightrider0704@gmail.com>
2021-05-21 18:13:30 -0500

    Dependency Injection

---
+++ Assets/ECSTesting/DebugOperations/PathDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace ECSTesting.DebugOperations
+{
+    using shipComps = ECSTesting.Components.Ships;
+    using sysComps = ECSTesting.Components.Systems;
+    using genComps = ECSTesting.Components;
+    class PathDisplay : IPathRenderer
+    {
+        public PathDisplay(Entity ship)
+        {
+            var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var queryDesc = new EntityQueryDesc() { All = new ComponentType[] { ComponentType.ReadOnly<sysComps.Id>(), ComponentType.ReadOnly<Translation>()}, None = new ComponentType[] { ComponentType.ReadOnly<genComps.CloneTag>()} };
+            var systemsQuery = em.CreateEntityQuery(queryDesc);
+            var sysIDs = systemsQuery.ToComponentDataArrayAsync<sysComps.Id>(Unity.Collections.Allocator.TempJob, out JobHandle handle);
+            var sysPos = systemsQuery.ToComponentDataArrayAsync<Translation>(Unity.Collections.Allocator.TempJob, out JobHandle handle2);
+
+            var buffer = em.GetBuffer<shipComps.WaypointBuffer>(ship);
+            var waypointsQuery = from item in buffer
+                            select item.data.wormholeToID;
+            var waypoints = waypointsQuery.ToArray();
+
+            for ( int i = 0; i < waypoints.Length-1; i++ )
+            {
+                var current = waypoints[i];
+                var target = waypoints[i + 1];
+
+                var currentPos = sysPos[current];
+                var targetPos = sysPos[target];
+
+                Debug.DrawLine(currentPos.Value, targetPos.Value);
+            }
+
+
+            sysIDs.Dispose();
+            sysPos.Dispose();
+        }
+    }
+
+    public interface IPathRenderer
+    {
+
+    }
+}
--- Assets/PlaymodeTests/PlaymodeTest.cs
+++ Assets/PlaymodeTests/PlaymodeTest.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using ECSTesting.Components;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Jobs;
+using System.Linq;
 
 public class PlaymodeTest
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void PlaymodeTestSimplePasses()
+    public void TestEntityOrder()
     {
+        var rand = new Unity.Mathematics.Random(1);
+        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var ents = new NativeArray<Entity>(100, Allocator.Temp);
+        var arch = em.CreateArchetype(typeof(SystemID), typeof(Translation));
+
+        em.CreateEntity(arch, ents);
+        foreach ( var ent in ents )
+        {
+            em.SetComponentData<SystemID>(ent, new SystemID() { id = rand.NextInt() });
+            em.SetComponentData<Translation>(ent, new Translation() { Value = rand.NextFloat3() });
+        }
+
+        var query = em.CreateEntityQuery(typeof(SystemID), typeof(Translation));
+
+        var entArray = query.ToEntityArrayAsync(Allocator.TempJob, out JobHandle handle1);
+        handle1.Complete();
+        var IDArray = query.ToComponentDataArrayAsync<SystemID>(Allocator.TempJob, out JobHandle handle2);
+        handle2.Complete();
+        var posArray = query.ToComponentDataArrayAsync<Translation>(Allocator.TempJob, out JobHandle handle3);
+        handle3.Complete();
+
+
+        var dataQuery = from ent in entArray.ToArray()
+                        from id in IDArray.ToArray()
+                        from pos in posArray.ToArray()
+                        where em.GetComponentData<SystemID>(ent).id == id.id
+                        where (em.GetComponentData<Translation>(ent).Value.x == pos.Value.x) && (em.GetComponentData<Translation>(ent).Value.y == pos.Value.y) && (em.GetComponentData<Translation>(ent).Value.z == pos.Value.z)
+                        select ent;
+
+        Assert.AreEqual(dataQuery.ToArray().Length, ents.Length);
+
+        entArray.Dispose();
+        IDArray.Dispose();
+        posArray.Dispose();
+        ents.Dispose();
         // Use the Assert class to test conditions
     }
 
@@ -18,8 +59,11 @@ public class PlaymodeTest
     [UnityTest]
     public IEnumerator PlaymodeTestWithEnumeratorPasses()
     {
+
+
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
+
         yield return null;
     }
 }
